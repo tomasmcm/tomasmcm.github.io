f1dac8a2c18fca9b2537686e1355c2813bd3e10c
tomasmcm <tomasmcm@users.noreply.github.com>
2022-12-20 14:41:36 +0000

    deploy: tomasmcm/portfolio-source@86779f14775715d865e529f6c4da8127406a2db0

--- assets/scripts/home.js
+++ assets/scripts/home.js
@@ -1,10 +1,1 @@
-var sr = ScrollReveal({
-  origin: "bottom",
-  distance: "64px",
-  duration: 800,
-  delay: 0,
-  scale: 1
-});
-
-sr.reveal(".projects-grid a");
-
+var sr=ScrollReveal({origin:"bottom",distance:"64px",duration:800,delay:0,scale:1});sr.reveal(".projects-grid a");
